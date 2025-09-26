648bf536c756beebe3edc3ecf31ca83035fc978d
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2025-09-25 21:45:52 -0300

    WIP: implemented fetch_package_resources

--- src/capes_etl/api/downloader.py
+++ src/capes_etl/api/downloader.py
@@ -22,6 +22,26 @@ def fetch_packages_ckan(url: str, query: str) -> list:
     results = data.get('result',{}).get('results',[])
     return results
 
+def fetch_package_resources(package_id: str) -> dict:
+    '''
+    _summary_
+
+    Args:
+        package_id (str): _description_
+
+    Returns:
+        dict: _description_
+    '''         
+    response = requests.get(
+        f'{CAPES_BASE_URL}/package_show',
+        params={'id': package_id}
+    )
+    response.raise_for_status()
+    data = response.json()
+    results = data.get('result',{}).get('resources',[])
+    return results
+
+
 results = fetch_packages_ckan(CAPES_BASE_URL, 'catalogo de teses')
 num_results = len(results)
 print(f'Found {num_results} entries')
@@ -30,3 +50,12 @@ for i, result in enumerate(results):
     print(f'Result {i}: {result['name']}\nID: {result['id']}\n')
 
 print(type(results))
+
+for k in results[0].keys():
+    print(k)
+
+package_id_tests = results[0]['id']
+print(package_id_tests)
+
+package_details = fetch_package_resources(package_id_tests)
+print(package_details)
